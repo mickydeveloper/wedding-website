3dad4d023e9b78837952271c959fd4520e44ea27
Mikolaj <mikolajhyzy95@gmail.com>
2017-06-09 14:10:20 +0200

    niit

--- functions.php
+++ functions.php
@@ -23,6 +23,6 @@ spl_autoload_register(function ($class) {
     require $file;
 });
 
-define('FEDERICO_NICOLE_VERSION', '1.0.4');
+define('FEDERICO_NICOLE_VERSION', '1.0.5');
 
 new \NuvoPoint\Themes\FedericoNicole\Init();
