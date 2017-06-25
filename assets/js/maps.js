babd48fef4536ed7a1c79958fc75b8299e17b6d9
Mikolaj <mikolajhyzy95@gmail.com>
2017-06-26 01:11:40 +0200

    wedding information page

---
+++ assets/js/maps.js
@@ -0,0 +1,41 @@
+/* The variables for the map init the styling of the map and the icon definition*/
+var mapStyles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}];
+var zoom = 14;
+var scrollwheel = false;
+
+/* We take all the init maps functions and pass them to the function which will be used as callback for the google maps api link */
+function initMaps() {
+    initMapChurch();
+    initMapVilla();
+}
+
+function initMapChurch() {
+    var mapDiv = document.getElementById('mapChurch');
+    var mapChurch = new google.maps.Map(mapDiv, {
+        center: {lat: 45.39828, lng: 11.94314},
+        zoom: zoom,
+        scrollwheel: scrollwheel,
+        styles: mapStyles
+    });
+    var marker = new google.maps.Marker({
+        position: {lat: 45.39828, lng: 11.94314},
+        icon: 'https://i0.wp.com/wp-blog-dir.s3.amazonaws.com/uploads/sites/5/2016/05/fav2.png',
+        map: mapChurch
+    });
+}
+
+function initMapVilla() {
+    var mapDiv = document.getElementById('mapVilla');
+    var mapVilla = new google.maps.Map(mapDiv, {
+        center: {lat: 45.1532333, lng: 11.7193155},
+        zoom: zoom,
+        scrollwheel: scrollwheel,
+        styles: mapStyles
+    });
+    var marker = new google.maps.Marker({
+        position: {lat: 45.1532333, lng: 11.7193155},
+        icon: 'https://i0.wp.com/wp-blog-dir.s3.amazonaws.com/uploads/sites/5/2016/05/fav2.png',
+        map: mapVilla
+    });
+
+}
--- src/Init.php
+++ src/Init.php
@@ -11,5 +11,7 @@ class Init
     public function scriptsAndStyles()
     {
         wp_enqueue_style('nuvofront-custom', get_stylesheet_directory_uri() . '/assets/scss/custom.css', ['nuvofront-common','mobile-nav', 'desktop-nav','nuvofront-grid'], FEDERICO_NICOLE_VERSION);
+        wp_enqueue_script('init_maps', get_stylesheet_directory_uri() . '/assets/js/maps.js', [], '1.0.0', true);
+        wp_enqueue_script('get_maps', '//maps.googleapis.com/maps/api/js?key=AIzaSyDQuYUx4Gv2jQPhIA5UIA31KD8_KY5wfpg&callback=initMaps', ['init_maps'], '1.0.0', true);
     }
 }
