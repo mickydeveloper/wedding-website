d345b05707d2a70cda2952a7e9d370dc41279991
Mikolaj <mikolajhyzy95@gmail.com>
2017-06-26 01:46:11 +0200

    change the styling

--- functions.php
+++ functions.php
@@ -23,6 +23,6 @@ spl_autoload_register(function ($class) {
     require $file;
 });
 
-define('FEDERICO_NICOLE_VERSION', '1.0.12');
+define('FEDERICO_NICOLE_VERSION', '1.0.13');
 
 new \NuvoPoint\Themes\FedericoNicole\Init();
