ca4c54b629dba9b7985b9373d7c7871cb2c0397f
Mikolaj <mikolajhyzy95@gmail.com>
2017-06-26 02:05:34 +0200

    update the site

--- functions.php
+++ functions.php
@@ -23,6 +23,6 @@ spl_autoload_register(function ($class) {
     require $file;
 });
 
-define('FEDERICO_NICOLE_VERSION', '1.0.14');
+define('FEDERICO_NICOLE_VERSION', '1.0.15');
 
 new \NuvoPoint\Themes\FedericoNicole\Init();
