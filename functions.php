92578998526c39ea9ab91b27b21d4f1ca3bce80b
Mikolaj <mikolajhyzy95@gmail.com>
2017-06-29 14:27:34 +0200

    Update the mobile styling

--- functions.php
+++ functions.php
@@ -23,6 +23,6 @@ spl_autoload_register(function ($class) {
     require $file;
 });
 
-define('FEDERICO_NICOLE_VERSION', '1.0.23');
+define('FEDERICO_NICOLE_VERSION', '1.0.24');
 
 new \NuvoPoint\Themes\FedericoNicole\Init();
