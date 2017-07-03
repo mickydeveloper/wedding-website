ae9c24a83e509086426306d8d0bd174949780ed0
Mikolaj <mikolajhyzy95@gmail.com>
2017-07-03 21:08:59 +0200

    change the settings

--- src/Init.php
+++ src/Init.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace NuvoPoint\Themes\FedericoNicole;
 
 class Init
@@ -10,8 +11,14 @@ class Init
 
     public function scriptsAndStyles()
     {
-        wp_enqueue_style('nuvofront-custom', get_stylesheet_directory_uri() . '/assets/scss/custom.css', ['nuvofront-common','mobile-nav', 'desktop-nav','nuvofront-grid'], FEDERICO_NICOLE_VERSION);
-        wp_enqueue_script('init_maps', get_stylesheet_directory_uri() . '/assets/js/maps.js', [], FEDERICO_NICOLE_VERSION, true);
-        wp_enqueue_script('get_maps', '//maps.googleapis.com/maps/api/js?key=AIzaSyDQuYUx4Gv2jQPhIA5UIA31KD8_KY5wfpg&callback=initMaps', ['init_maps'], FEDERICO_NICOLE_VERSION, true);
+        wp_enqueue_style('nuvofront-custom', get_stylesheet_directory_uri() . '/assets/scss/custom.css',
+            ['nuvofront-common', 'mobile-nav', 'desktop-nav', 'nuvofront-grid'], FEDERICO_NICOLE_VERSION);
+        if (is_page("Confirmation")) {
+            wp_enqueue_script('init_maps', get_stylesheet_directory_uri() . '/assets/js/maps.js', [],
+                FEDERICO_NICOLE_VERSION, true);
+            wp_enqueue_script('get_maps',
+                '//maps.googleapis.com/maps/api/js?key=AIzaSyDQuYUx4Gv2jQPhIA5UIA31KD8_KY5wfpg&callback=initMaps',
+                ['init_maps'], FEDERICO_NICOLE_VERSION, true);
+        }
     }
 }
